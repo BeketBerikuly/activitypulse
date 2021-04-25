2b854a0b1712adfa96ba8c5a0bf9b30ee455caf6
Beket <180107212@stu.sdu.edu.kz>
2021-04-25 22:55:51 +0600

    Update config.php

--- includes/php/config.php
+++ includes/php/config.php
@@ -1,7 +1,15 @@
 <?php
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_DATABASE', 'se');
+// Development connection
+// define('DB_SERVER', 'localhost');
+// define('DB_USERNAME', 'root');
+// define('DB_PASSWORD', '');
+// define('DB_DATABASE', 'se');
+
+// Remote database coonection
+define('DB_SERVER', 'remotemysql.com');
+define('DB_USERNAME', 'O131yK0GMe');
+define('DB_PASSWORD', 'Dnclv4C2sS');
+define('DB_DATABASE', 'O131yK0GMe');
+
 
 $db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
